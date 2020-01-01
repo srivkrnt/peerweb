617409aea647a2e7fd228e1f553d41acaad97ad5
srivkrnt <sri.vkrnt@gmail.com>
2020-01-01 12:37:17 +0530

    Debug-false, config-update

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 A system built using flask, implementing peer to peer based services using REST APIs.
 
+Set your IP and Port in the config.py to test the application
--- manage.py
+++ manage.py
@@ -66,4 +66,4 @@ def status():
     return render_template('status.html')
 
 if __name__ == "__main__":
-    app.run(debug=True, host = '0.0.0.0')
+    app.run(host = '0.0.0.0')
